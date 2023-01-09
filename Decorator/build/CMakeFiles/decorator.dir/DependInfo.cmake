a68c2cd9322a5232ab3122228762608554050026
brunoleppe <bruno.leppe.dev@gmail.com>
2023-01-08 22:05:28 -0500

    Added Decorator Pattern. Fixed overriding inherited interfaces: children not creating a new iface_entry array so overriding overrides all shared methods

---
+++ Decorator/build/CMakeFiles/decorator.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/armordecorator.c" "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/build/CMakeFiles/decorator.dir/armordecorator.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/baseenemy.c" "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/build/CMakeFiles/decorator.dir/baseenemy.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/decorator.c" "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/build/CMakeFiles/decorator.dir/decorator.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/enemydecorator.c" "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/build/CMakeFiles/decorator.dir/enemydecorator.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/ienemy.c" "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/build/CMakeFiles/decorator.dir/ienemy.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/shielddecorator.c" "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/build/CMakeFiles/decorator.dir/shielddecorator.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bruno/Documents/Github/Design-Patterns-C/Decorator/build/blib/CMakeFiles/blib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Decorator/build/CMakeFiles/decorator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decorator.dir/armordecorator.c.o"
+  "CMakeFiles/decorator.dir/baseenemy.c.o"
+  "CMakeFiles/decorator.dir/decorator.c.o"
+  "CMakeFiles/decorator.dir/enemydecorator.c.o"
+  "CMakeFiles/decorator.dir/ienemy.c.o"
+  "CMakeFiles/decorator.dir/shielddecorator.c.o"
+  "decorator"
+  "decorator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/decorator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
