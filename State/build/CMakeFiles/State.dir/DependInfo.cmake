a4f592e99d06e512b0760fd0a8f282dc0701468e
brunoleppe <bruno.leppe.dev@gmail.com>
2023-01-11 22:13:17 -0500

    Added State + Singleton Pattern

---
+++ State/build/CMakeFiles/State.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bruno/Documents/Github/Design-Patterns-C/State/bstate.c" "/home/bruno/Documents/Github/Design-Patterns-C/State/build/CMakeFiles/State.dir/bstate.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/State/endstate.c" "/home/bruno/Documents/Github/Design-Patterns-C/State/build/CMakeFiles/State.dir/endstate.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/State/initstate.c" "/home/bruno/Documents/Github/Design-Patterns-C/State/build/CMakeFiles/State.dir/initstate.c.o"
+  "/home/bruno/Documents/Github/Design-Patterns-C/State/state.c" "/home/bruno/Documents/Github/Design-Patterns-C/State/build/CMakeFiles/State.dir/state.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../bObject"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bruno/Documents/Github/Design-Patterns-C/State/build/blib/CMakeFiles/blib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ State/build/CMakeFiles/state.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/state.dir/bstate.c.o"
+  "CMakeFiles/state.dir/endstate.c.o"
+  "CMakeFiles/state.dir/initstate.c.o"
+  "CMakeFiles/state.dir/state.c.o"
+  "state"
+  "state.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/state.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
